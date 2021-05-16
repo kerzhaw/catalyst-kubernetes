5c2c31e9235793ea941444f61bf5df421104a30d
Paul Kershaw <7711632+kerzhaw@users.noreply.github.com>
2021-05-16 23:42:11 +1200

    Done

--- controller-setup.sh
+++ controller-setup.sh
@@ -56,3 +56,9 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable etcd
 sudo systemctl start etcd
+
+sudo ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kubernetes.pem \
+  --key=/etc/etcd/kubernetes-key.pem
