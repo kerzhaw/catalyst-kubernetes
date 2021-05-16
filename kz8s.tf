d94a86f8809ee4fae26924adbd0b94248ed6f0f3
Paul Kershaw <7711632+kerzhaw@users.noreply.github.com>
2021-05-16 16:06:32 +1200

    Versions

--- kz8s.tf
+++ kz8s.tf
@@ -1,10 +1,10 @@
 # Define required providers
 terraform {
-required_version = ">= 0.14.0"
+required_version = ">= 0.15.3"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.35.0"
+      version = "~> 1.42.0"
     }
   }
 }
