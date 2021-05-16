14a21f14c2f376f974e2b0cbc93ed2e010bf09ab
Paul Kershaw <7711632+kerzhaw@users.noreply.github.com>
2021-05-16 17:07:58 +1200

    updates

--- kz8s.tf
+++ kz8s.tf
@@ -9,7 +9,7 @@ required_version = ">= 0.15.3"
   }
 }
 
-provider "openstack" {
+provider "openstack" {  
 }
 
 # Create a Router
@@ -53,4 +53,38 @@ resource "openstack_compute_secgroup_v2" "kubernetes-SSH" {
         ip_protocol = "tcp"
         cidr = "0.0.0.0/0"
     }
+}
+
+# Upload SSH public key
+# replace public_key with a valid SSH public key that you wish to use
+resource "openstack_compute_keypair_v2" "keypair" {
+  name = "kz8s"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDofvDyT4t1HaJHCADt3ljS5Q5usPm2za7dieyklwVaZ63PDk9eYsh2/VezojcxfB8fgd/IR5RXU1TfYcigH0/Z3TxHyXxfCb3E9BbdIqJ/IRVAOHEAL8EP1lQNFEYtLe3g4JE0Jeicxn9wFNIDO8nrcGOGyblHaSq1K3o5wt81n0D4t7T1nNp42/QSi9s15JucIMVn8a+sYYT7wABtETANRC8M2aXRwCRCFomuGhQ3ZDreTCR/LylfQEMUSwSZUzO2QoReFP9bN/9j74c/iZOSRyQguD796EmkGsQHbsdNFtJiB2cSg9dbESoYx4Sqk9RUBWyNg2eav0uxC+A+wN49p0VbJZ7tFNC/Dd0PX82iBS0BTXGqagIsT5MG5oigbkOuI4r3vpAPcVn7QCo1qRoIMMF6GwuoSFFPYwKIjMUcTahSHDAMFgmVjvmYOoTKJglJvrLmgq7L9dTPgcxB46t0xkXhof+0HTM1m1Qc8oXLJLZlqabRpFKArbTP5+RTw6E= paul@kerzhaw.com"
+}
+
+resource "openstack_blockstorage_volume_v2" "controller-0-volume" {
+  name        = "controller-0-volume"
+  size        = 100
+  image_id    = "e4623196-706d-4d1e-8f42-769b2300650c" // ubuntu-20.04-x86_64
+}
+
+## Create controller-0
+resource "openstack_compute_instance_v2" "controller-0" {
+  name            = "controller-0"
+  flavor_id       = "c62db2ea-2ccd-49f0-9b88-307cdd3d6e0e" // c1.c2r2
+  key_pair        = "kz8s"
+  security_groups = ["${openstack_compute_secgroup_v2.kubernetes-SSH.name}", "default"]
+
+  block_device {
+    uuid                  = "${openstack_blockstorage_volume_v2.controller-0-volume.id}"
+    source_type           = "volume"
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = false
+  }
+
+  network {
+    name = "${openstack_networking_network_v2.kz8s-net.name}"
+    fixed_ip_v4 = "10.240.0.10"
+  }
 }
