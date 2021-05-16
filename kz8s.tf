cbfa6e874ac82ab8c48735125b6eaec13425f1ab
Paul Kershaw <7711632+kerzhaw@users.noreply.github.com>
2021-05-16 19:48:18 +1200

    try this

--- kz8s.tf
+++ kz8s.tf
@@ -1,6 +1,6 @@
 # Define required providers
 terraform {
-required_version = ">= 0.15.3"
+  required_version = ">= 0.15.3"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
@@ -9,7 +9,7 @@ required_version = ">= 0.15.3"
   }
 }
 
-provider "openstack" {  
+provider "openstack" {
 }
 
 # Create a Router
@@ -24,17 +24,17 @@ locals {
 
 # Create a Network  
 resource "openstack_networking_network_v2" "kz8s-net" {
-    name = "kz8s-net"
-    admin_state_up = "true"
+  name           = "kz8s-net"
+  admin_state_up = "true"
 }
 
 # Create a Subnet
 resource "openstack_networking_subnet_v2" "kubernetes-subnet" {
-    name = "kubernetes-subnet"
-    network_id = "${openstack_networking_network_v2.kz8s-net.id}"
-    enable_dhcp = "true"
-    cidr = "10.240.0.0/24"
-    ip_version = 4
+  name        = "kubernetes-subnet"
+  network_id  = openstack_networking_network_v2.kz8s-net.id
+  enable_dhcp = "true"
+  cidr        = "10.240.0.0/24"
+  ip_version  = 4
 }
 
 # Create a Router interface(s)
@@ -44,33 +44,33 @@ resource "openstack_networking_subnet_v2" "kubernetes-subnet" {
 #}
 
 resource "openstack_networking_router_interface_v2" "kz8s-interface" {
-    router_id = "${local.border_router_id}"
-    subnet_id = "${openstack_networking_subnet_v2.kubernetes-subnet.id}"
+  router_id = local.border_router_id
+  subnet_id = openstack_networking_subnet_v2.kubernetes-subnet.id
 }
 
 # Create a Security Group
 resource "openstack_compute_secgroup_v2" "kubernetes-SSH" {
-    name = "kubernetes-SSH"
-    description = "kubernetes SSH"
-    rule {
-        from_port = 22
-        to_port = 22
-        ip_protocol = "tcp"
-        cidr = "192.168.0.0/24"
-    }
+  name        = "kubernetes-SSH"
+  description = "kubernetes SSH"
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "192.168.0.0/24"
+  }
 }
 
 # Upload SSH public key
 # replace public_key with a valid SSH public key that you wish to use
 resource "openstack_compute_keypair_v2" "keypair" {
-  name = "kz8s"
+  name       = "kz8s"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDofvDyT4t1HaJHCADt3ljS5Q5usPm2za7dieyklwVaZ63PDk9eYsh2/VezojcxfB8fgd/IR5RXU1TfYcigH0/Z3TxHyXxfCb3E9BbdIqJ/IRVAOHEAL8EP1lQNFEYtLe3g4JE0Jeicxn9wFNIDO8nrcGOGyblHaSq1K3o5wt81n0D4t7T1nNp42/QSi9s15JucIMVn8a+sYYT7wABtETANRC8M2aXRwCRCFomuGhQ3ZDreTCR/LylfQEMUSwSZUzO2QoReFP9bN/9j74c/iZOSRyQguD796EmkGsQHbsdNFtJiB2cSg9dbESoYx4Sqk9RUBWyNg2eav0uxC+A+wN49p0VbJZ7tFNC/Dd0PX82iBS0BTXGqagIsT5MG5oigbkOuI4r3vpAPcVn7QCo1qRoIMMF6GwuoSFFPYwKIjMUcTahSHDAMFgmVjvmYOoTKJglJvrLmgq7L9dTPgcxB46t0xkXhof+0HTM1m1Qc8oXLJLZlqabRpFKArbTP5+RTw6E= paul@kerzhaw.com"
 }
 
 resource "openstack_blockstorage_volume_v2" "controller-0-volume" {
-  name        = "controller-0-volume"
-  size        = 100
-  image_id    = "e4623196-706d-4d1e-8f42-769b2300650c" // ubuntu-20.04-x86_64
+  name     = "controller-0-volume"
+  size     = 100
+  image_id = "e4623196-706d-4d1e-8f42-769b2300650c" // ubuntu-20.04-x86_64
 }
 
 ## Create controller-0
@@ -81,7 +81,7 @@ resource "openstack_compute_instance_v2" "controller-0" {
   security_groups = ["${openstack_compute_secgroup_v2.kubernetes-SSH.name}", "default"]
 
   block_device {
-    uuid                  = "${openstack_blockstorage_volume_v2.controller-0-volume.id}"
+    uuid                  = openstack_blockstorage_volume_v2.controller-0-volume.id
     source_type           = "volume"
     boot_index            = 0
     destination_type      = "volume"
@@ -89,25 +89,37 @@ resource "openstack_compute_instance_v2" "controller-0" {
   }
 
   network {
-    name = "${openstack_networking_network_v2.kz8s-net.name}"
+    name        = openstack_networking_network_v2.kz8s-net.name
     fixed_ip_v4 = "10.240.0.10"
   }
 
-#   provisioner "local-exec" {
-#     command = "echo ${self.name} >> what_is_my_name.txt"
-#   }
+  #   provisioner "file" {
+  #     source      = "controller-setup.sh"    
+  #     destination = "~/controller-setup.sh"
 
-  provisioner "file" {
-    source      = "testfile.txt"    
-    destination = "~/testfile.txt"
+  #     connection {
+  #       type     = "ssh"
+  #       user     = "ubuntu"
+  #       private_key = file("../kz8s.key")
+  #       host     = "${openstack_compute_instance_v2.controller-0.network[0].fixed_ip_v4}"
+  #     }
+  #   }
+
+  #    provisioner "local-exec" {
+  #      command = "bash ~/controller-setup.sh"
+  #    }
+
+  provisioner "remote-exec" {
+    script = "controller-setup.sh"
 
     connection {
-      type     = "ssh"
-      user     = "ubuntu"
+      type        = "ssh"
+      user        = "ubuntu"
       private_key = file("../kz8s.key")
-      host     = "${openstack_compute_instance_v2.controller-0.network[0].fixed_ip_v4}"
+      host        = "${openstack_compute_instance_v2.controller-0.network[0].fixed_ip_v4}"
     }
   }
+
 }
 
 # # Request a floating IP
